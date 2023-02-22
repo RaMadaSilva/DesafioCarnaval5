c8a8b066e981e461b39a0a4a70340afd438188b1
RaMadaSilva <raul01mateia@gmail.com>
2023-02-22 02:27:25 +0100

    melhorando a saida dos valores

--- Conversor.cs
+++ Conversor.cs
@@ -6,18 +6,18 @@ using System.Text;
 namespace DesafioCarnaval5;
 public class Conversor
 {
-    private static readonly Dictionary<double, string> unidade = new Dictionary<double, string>{
+    private static readonly Dictionary<double, string> primeiraOrdem = new Dictionary<double, string>{
         {0, "zero"}, {1, "um"}, {2, "Dois"}, {3, "três"}, {4, "quatro"}, {5, "cinco"},
-        {6, "seis"}, {7, "sete"}, {8, "oito"}, {9, "nove"},  {11, "onze"}, {12, "doze"}, 
+        {6, "seis"}, {7, "sete"}, {8, "oito"}, {9, "nove"},{10, "dez"}, {11, "onze"}, {12, "doze"}, 
         {13, "treze"}, {14, "catorze"}, {15, "quinze"},{16, "dezesseis"}, {17, "dezessete"}, 
         {18, "dezoito"}, {19, "dezenove"}};
-    private static readonly Dictionary<double, string> dezena = new Dictionary<double, string>{
-        {10, "dez"}, {20, "vinte"}, {30, "trinta"},{40, "quarenta"}, {50, "cinquenta"}, {60, "secenta"}, 
+    private static readonly Dictionary<double, string> segundaOrdem = new Dictionary<double, string>{
+        {20, "vinte"}, {30, "trinta"},{40, "quarenta"}, {50, "cinquenta"}, {60, "secenta"}, 
         {70, "setenta"}, {80, "oitenta"}, {90, "noventa"}};
-    private static readonly Dictionary<double, string> centena = new Dictionary<double, string>{
+    private static readonly Dictionary<double, string> terceiraOrdem = new Dictionary<double, string>{
         {100, "cem"}, {200, "duzentos"}, {300, "trezentos"}, {400, "quatrocentos"}, {500, "quinhentos"},
         {600, "seicentos"}, {700, "setecentos"}, {800, "oitocentos"}, {900, "novecentos"}};
-    private static readonly Dictionary<double, string> milhar = new Dictionary<double, string>{
+    private static readonly Dictionary<double, string> quartaOrdem = new Dictionary<double, string>{
         {1000, "mil"}, {2000, "dois mil"}, {3000, "tres mil"}, {4000, "quatro mil"}, {5000, "cinco mil"},
         {6000, "seis mil"}, {7000, "sete mil"}, {8000, "oito mil"}, {9000, "nove mil"}, {10000, "dez mil"}};
 
@@ -43,10 +43,10 @@ public class Conversor
         {
             if (valorMilhar!=0 && restoMilhar % 100 == 0)
             {
-                foreach (var item in milhar.Keys)
+                foreach (var item in quartaOrdem.Keys)
                 {
                     if (valor == item)
-                       return sb.Append($"{milhar[item]}").ToString();
+                       return sb.Append($"{quartaOrdem[item]}").ToString();
                 }
             }
             else
@@ -57,43 +57,68 @@ public class Conversor
 
                 if (valorCentena!=0 && restoCentena % 10 == 0)
                 {
-                    foreach (var item in centena.Keys)
+                    foreach (var item in terceiraOrdem.Keys)
                     {
                         if (valorCentena * 100 == item)
-                            return sb.Append($"{milhar[valorMilhar*1000]} e {centena[item]}").ToString();
+                            return sb.Append($"{quartaOrdem[valorMilhar*1000]} e {terceiraOrdem[item]}").ToString();
                     }
                 }
                 else
                 {
                     sobra = restoCentena;
-                    valorDesena = (int)(sobra / 10);
-                    restoDezena = (int)(sobra % 10);
-
-                    if (valorDesena!=0 && restoDezena == 0)
+                    if (sobra > 19)
                     {
-                        foreach (var item in dezena.Keys)
+                        valorDesena = (int)(sobra / 10);
+                        restoDezena = (int)(sobra % 10);
+
+                        if (valorDesena != 0 && restoDezena % 10 == 0)
                         {
-                            if (sobra == item)
-                                return sb.Append($"{milhar[valorMilhar * 1000]} e {centena[valorCentena * 100]} e {dezena[item]}").ToString();
+                            foreach (var item in segundaOrdem.Keys)
+                            {
+                                if (sobra == item)
+                                {
+                                    var mil = valorMilhar == 0 ? string.Empty : (quartaOrdem[valorMilhar * 1000]);
+                                    var cent = (valorCentena == 0) ? string.Empty : (terceiraOrdem[valorCentena * 100]);
+                                    return sb.Append($"{mil} e {cent} e {segundaOrdem[item]}").ToString();
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            sobra = restoDezena;
+                            foreach (var item in primeiraOrdem.Keys)
+                            {
+
+                                if (sobra == item)
+                                {
+                                    valUnite = primeiraOrdem[item].ToString();
+                                    var mil = (valorMilhar == 0) ? string.Empty : quartaOrdem[valorMilhar * 1000];
+                                    var cent = (valorCentena == 0) ? string.Empty : terceiraOrdem[valorCentena * 100] + " e ";
+                                    var dezn = (valorDesena == 0) ? string.Empty : segundaOrdem[valorDesena * 10] + " e ";
+                                    return sb.Append($"{mil} {cent}  {dezn} {valUnite}").ToString().Trim();
+
+                                }
+                            }
                         }
                     }
                     else
                     {
-                        sobra = restoDezena;
-                        foreach (var item in unidade.Keys)
+                        //sobra = restoDezena;
+                        foreach (var item in primeiraOrdem.Keys)
                         {
-        
+
                             if (sobra == item)
                             {
-                                valUnite = unidade[item].ToString();
-                                var mil = (valorMilhar == 0) ? string.Empty : milhar[valorMilhar * 1000];
-                                var cent = (valorCentena == 0) ? string.Empty : centena[valorCentena * 100] + " e ";
-                                var dezn = (valorDesena == 0) ? string.Empty : dezena[valorDesena * 10] + " e ";
+                                valUnite = primeiraOrdem[item].ToString();
+                                var mil = (valorMilhar == 0) ? string.Empty : quartaOrdem[valorMilhar * 1000];
+                                var cent = (valorCentena == 0) ? string.Empty : terceiraOrdem[valorCentena * 100] + " e ";
+                                var dezn = (valorDesena == 0) ? string.Empty : segundaOrdem[valorDesena * 10] + " e ";
                                 return sb.Append($"{mil} {cent}  {dezn} {valUnite}").ToString().Trim();
-                               
+
                             }
                         }
- 
+
                     }
 
                 }
